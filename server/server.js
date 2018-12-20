071048d2fdefa6abead8722d02934fd307be9de0
Angelino Vera <gienovera@gmail.com>
2018-12-21 00:39:55 +0800

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.listen(3000, () => {
+    console.log(`Server is up on port 3000`)
+})
